cbabafa64faac069722c827337bf24ea472c64ed
phucvinh57 <nguyenphucvinh05072001@gmail.com>
2021-01-13 22:41:41 +0700

    update

--- raw_graph_4.py
+++ raw_graph_4.py
@@ -7,31 +7,36 @@ import numpy as np
 def msg(a):
     n = 0
     for idx in range(0, 2013):
-        n += abs(float(df.at[idx, a]) - float(de.at[idx, 'CO2Air_Real']))
+        n += math.pow((float(df.at[idx, a]) - float(de.at[idx, 'CO2Air_Real'])),2)
     return n/2013
 
-data = pd.read_excel("Output.xlsx")
+data = pd.read_excel("Output_4.xlsx")
 df = pd.DataFrame(data)
 da = pd.read_excel("data.xlsx")
 de = pd.DataFrame(da)
 time = df[['time']]
 euler_air = df[['CO2Air_E']]
 real_co2 = de[['CO2Air_Real']]
+rk4_air = df[['CO2Air_RK4']]
 
 print(msg('CO2Air_E'))
+print(msg('CO2Air_RK4'))
+
 plt.plot(time, euler_air, label='Euler')
 plt.plot(time, real_co2 , label='Thực Tế')
 plt.xlabel('Time')
 plt.ylabel('CO2Air')
+plt.title("Biểu đồ CO2Air đo bằng phương pháp Euler và CO2Air thực tế")
 plt.legend(loc='best')
 plt.show()
 
-rk4_air = df[['CO2Air_RK4']]
-print(msg('CO2Air_RK4'))
+
+
 plt.plot(time, rk4_air, 'b' , label='RK4')
 plt.plot(time, real_co2, 'r' ,label='Thực Tế')
 plt.xlabel('Time')
 plt.ylabel('CO2Air')
+plt.title("Biểu đồ CO2Air đo bằng phương pháp RK4 và CO2Air thực tế")
 plt.legend(loc='best')
 plt.show()
 
@@ -39,6 +44,7 @@ euler_top = df[['CO2Top_E']]
 plt.plot(time, euler_top , label='Euler')
 plt.xlabel('Time')
 plt.ylabel('CO2Top')
+plt.title("Biểu đồ CO2Top đo bằng phương pháp Euler")
 plt.legend(loc='best')
 plt.show()
 
@@ -46,5 +52,6 @@ rk4_top = df[['CO2Top_RK4']]
 plt.plot(time, rk4_top , label='RK4')
 plt.xlabel('Time')
 plt.ylabel('CO2Top')
+plt.title("Biểu đồ CO2Top đo bằng phương pháp RK4")
 plt.legend(loc='best')
 plt.show()
